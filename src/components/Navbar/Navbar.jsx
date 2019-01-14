55a4f5b2926a4f22f715180b94c4e35d44a0095c
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-01-14 21:27:08 +0200

    Fixed a bug with overflowing modals on mobile due to the animation of the Navbar. We're now animating it with react-spring.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Link from 'react-router-dom/Link';
 import styled from 'styled-components';
+import { Transition } from 'react-spring';
 import { Fab, Button } from 'already-styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -15,20 +16,18 @@ const FadedFab = styled(Fab)`
   }
 `;
 const SideMenu = styled.div`
-  font-family: 'Open Sans', sans-serif;
-  font-size: 2.5rem;
-  position: fixed;
   display: flex;
+  position: fixed;
+  top: 0;
+  right: 0;
+  font-size: 2.5rem;
   flex-direction: column;
   text-align: right;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
   padding: 20px;
   background-color: #222222;
-  top: 0;
-  right: -320px;
   width: 320px;
   height: 100vh;
-  transition: right 0.33s ease-out;
   z-index: 12;
 
   a {
@@ -39,12 +38,11 @@ const SideMenu = styled.div`
 `;
 const SideMenuBackground = styled.div`
   position: fixed;
-  display: none;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.4);
   z-index: 11;
 `;
 
@@ -70,24 +68,24 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
 
-    this.sideMenuRef = React.createRef();
-    this.sideMenuBgRef = React.createRef();
+    this.state = {
+      mobileMenuOpen: false,
+    };
 
     this.openSlideMenu = this.openSlideMenu.bind(this);
     this.closeSlideMenu = this.closeSlideMenu.bind(this);
   }
 
   openSlideMenu() {
-    this.sideMenuBgRef.current.style.display = 'block';
-    this.sideMenuRef.current.style.right = '0px';
+    this.setState({ mobileMenuOpen: true });
   }
 
   closeSlideMenu() {
-    this.sideMenuBgRef.current.style.display = 'none';
-    this.sideMenuRef.current.style.right = '-320px';
+    this.setState({ mobileMenuOpen: false });
   }
 
   render() {
+    const { mobileMenuOpen } = this.state;
     return (
       <nav>
         <FadedFab
@@ -103,24 +101,36 @@ class Navbar extends Component {
           <FontAwesomeIcon icon={faBars} />
         </FadedFab>
 
-        <SideMenu ref={this.sideMenuRef}>
-          <CloseButton
-            transparent
-            fs="50px"
-            c="beige"
-            aria-label="close sidebar"
-            onClick={this.closeSlideMenu}
-          >
-            <FontAwesomeIcon icon={faTimes} />
-          </CloseButton>
-          <Link to="/" style={{ marginTop: '50px' }}>
-            Home
-          </Link>
-          <Link to="/faq/">FAQ</Link>
-          <Link to="/new-features/">Νew Features</Link>
-        </SideMenu>
-
-        <SideMenuBackground ref={this.sideMenuBgRef} onClick={this.closeSlideMenu} />
+        <Transition
+          items={mobileMenuOpen}
+          from={{ transform: 'translateX(320px)', opacity: 0 }}
+          enter={{ transform: 'translateX(0)', opacity: 1 }}
+          leave={{ transform: 'translateX(320px)', opacity: 0 }}
+        >
+          {show => show
+            && (({ transform, opacity }) => (
+              <>
+                <SideMenu style={{ transform }}>
+                  <CloseButton
+                    transparent
+                    fs="50px"
+                    c="beige"
+                    aria-label="close sidebar"
+                    onClick={this.closeSlideMenu}
+                  >
+                    <FontAwesomeIcon icon={faTimes} />
+                  </CloseButton>
+                  <Link to="/" style={{ marginTop: '50px' }}>
+                    Home
+                  </Link>
+                  <Link to="/faq/">FAQ</Link>
+                  <Link to="/new-features/">Νew Features</Link>
+                </SideMenu>
+                <SideMenuBackground style={{ opacity }} onClick={this.closeSlideMenu} />
+              </>
+            ))
+          }
+        </Transition>
       </nav>
     );
   }
