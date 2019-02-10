dd4a329ec0ad1c5e46558cebc2dcc8f3ccdbf61d
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-02-10 17:48:53 +0200

    Added hooks in App #2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Transition } from 'react-spring';
 import ReactGA from 'react-ga';
 import Loadable from 'react-loadable';
+import sortBy from 'lodash.sortby';
 
 import GlobalStyle from './AppGlobalStyle';
 import Loading from './components/Loading/Loading';
@@ -25,112 +26,92 @@ const LoadableNewFeatures = Loadable({
   loading: Loading,
 });
 
-const sortBy = require('lodash.sortby');
+const App = () => {
+  const heroCodes = [274, 31, 637, 671, 813, 930, 1066, 893, 7];
+  const dataVersion = 27;
 
-export default class App extends Component {
-  constructor() {
-    super();
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [state, setState] = useState({
+    cards: [],
+    heroes: [],
+    heroPowers: [],
+    archetypes: [],
+    extraDeckWideFilters: [],
+  });
+  const {
+    cards, heroes, heroPowers, archetypes, extraDeckWideFilters,
+  } = state;
 
-    this.heroCodes = [274, 31, 637, 671, 813, 930, 1066, 893, 7];
-
-    this.state = {
-      dataVersion: 27, // REMEMBER TO CHANGE THIS WHENEVER WE CHANGE THE DATA. :)
-      errorMessage: null,
-      // External data.
-      cards: [],
-      heroes: [],
-      heroPowers: [],
-      archetypes: [],
-      extraDeckWideFilters: [],
-    };
-  }
-
-  componentDidMount() {
-    const { dataVersion } = this.state;
-
+  useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       ReactGA.initialize(process.env.REACT_APP_GA_PROPERTY);
     }
 
     const cachedData = localStorage.getItem('data');
     const cachedVersion = parseInt(localStorage.getItem('version'), 10);
-    if (cachedData && cachedVersion === dataVersion) this.setData(JSON.parse(cachedData));
-    else this.fetchData();
-  }
-
-  setData(data) {
-    this.setState({
+    const setData = data => setState({
       cards: data[0].content,
       heroes: data[2].content,
       heroPowers: data[3].content,
       archetypes: sortBy(data[1].content, 'name'),
       extraDeckWideFilters: sortBy(data[4].content, ['group']),
     });
-  }
+    if (cachedData && cachedVersion === dataVersion) setData(JSON.parse(cachedData));
+    else {
+      fetch('/resources/data/data.json')
+        .then(response => response.json())
+        .then((data) => {
+          localStorage.setItem('data', JSON.stringify(data));
+          localStorage.setItem('version', dataVersion);
+          setData(data);
+        })
+        .catch(error => setErrorMessage(error.message));
+    }
+  }, []);
 
-  fetchData() {
-    const { dataVersion } = this.state;
-    fetch('/resources/data/data.json')
-      .then(response => response.json())
-      .then((data) => {
-        localStorage.setItem('data', JSON.stringify(data));
-        localStorage.setItem('version', dataVersion);
-        this.setData(data);
-      })
-      .catch(error => this.setState({ errorMessage: error.message }));
-  }
-
-  render() {
-    const {
-      cards,
-      heroes,
-      heroPowers,
-      archetypes,
-      extraDeckWideFilters,
-      errorMessage,
-    } = this.state;
-    return (
-      <>
-        <GlobalStyle />
-        <Transition
-          items={errorMessage}
-          from={{ transform: 'translateY(-100px)' }}
-          enter={{ transform: 'translateY(0)' }}
-          leave={{ transform: 'translateY(-100px)' }}
-        >
-          {toggle => toggle
-            && (style => (
-              <Alert
-                message={errorMessage}
-                callback={() => this.setState({ errorMessage: '' })}
-                timeout={10000}
-                style={style}
-              />
-            ))
-          }
-        </Transition>
-        <Router>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <LoadableHome
-                  cards={cards}
-                  heroes={heroes}
-                  heroPowers={heroPowers}
-                  heroCodes={this.heroCodes}
-                  archetypes={archetypes}
-                  extraDeckWideFilters={extraDeckWideFilters}
-                />
-              )}
+  return (
+    <>
+      <GlobalStyle />
+      <Transition
+        items={errorMessage}
+        from={{ transform: 'translateY(-100px)' }}
+        enter={{ transform: 'translateY(0)' }}
+        leave={{ transform: 'translateY(-100px)' }}
+      >
+        {toggle => toggle
+          && (style => (
+            <Alert
+              message={errorMessage}
+              callback={() => setErrorMessage('')}
+              timeout={10000}
+              style={style}
             />
-            <Route path="/faq/" render={() => <LoadableFAQ />} />
-            <Route path="/new-features/" render={() => <LoadableNewFeatures />} />
-            <Route render={() => <LoadableNotFound />} />
-          </Switch>
-        </Router>
-      </>
-    );
-  }
-}
+          ))
+        }
+      </Transition>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <LoadableHome
+                cards={cards}
+                heroes={heroes}
+                heroPowers={heroPowers}
+                heroCodes={heroCodes}
+                archetypes={archetypes}
+                extraDeckWideFilters={extraDeckWideFilters}
+              />
+            )}
+          />
+          <Route path="/faq/" render={() => <LoadableFAQ />} />
+          <Route path="/new-features/" render={() => <LoadableNewFeatures />} />
+          <Route render={() => <LoadableNotFound />} />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default App;
